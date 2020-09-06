f2dc0da081169fdfd7f38d3be03bf615c7a4829a
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-09-06 09:39:00 -0500

    js-router

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,46 @@
 import React from 'react';
-import firebase from 'firebase';
+import {
+  Route,
+  Switch,
+  Redirect,
+  BrowserRouter,
+} from 'react-router-dom';
+import firebase from 'firebase/app';
 import 'firebase/auth';
+
+// import Home from '../components/Home/Home';
+import New from '../components/New/New';
+import Home from '../components/Home/Home';
+import MyStuff from '../components/MyStuff/MyStuff';
+import MyNavbar from '../components/Navbar/Navbar';
+import Edit from '../components/Edit/Edit';
+import SingleStuff from '../components/SingleStuff/SingleStuff';
+import Login from '../components/Login/Login';
 
 import fbConnection from '../helpers/data/connection';
 
 import './App.scss';
-import MyNavbar from '../Components/Navbar/Navbar';
 
 fbConnection();
 
+const PublicRoute = ({ component: Component, ...rest }) => {
+  const routeChecker = (props) => (
+    props.authed
+      ? (<Component {...props} />)
+      : (<Redirect to={{ pathname: '/', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const routeChecker = (props) => (
+    props.authed
+      ? (<Component {...props} />)
+      : (<Redirect to={{ pathname: '/login', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
 class App extends React.Component {
-  state = {
-    authed: false,
-  }
+  state = { authed: false }
 
   componentDidMount() {
     this.removeListener = firebase.auth().onAuthStateChanged((user) => {
@@ -28,7 +56,21 @@ class App extends React.Component {
     const { authed } = this.state;
     return (
       <div className="App">
-        <MyNavbar authed={authed}/>
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavbar authed={authed} />
+            <div className="container">
+            <Switch>
+              <PublicRoute path='/login' component={Login} authed={authed} />
+              <PrivateRoute path='/new' component={New} authed={authed} />
+              <PrivateRoute path='/stuff' component={MyStuff} authed={authed} />
+              <PrivateRoute path='/singleStuff/:stuffId' component={SingleStuff} authed={authed} />
+              <PrivateRoute path='/edit/:stuffId' component={Edit} authed={authed} />
+              <PublicRoute path='/home' component={Home} authed={authed} exact />
+            </Switch>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/Components/New/New.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class New extends React.Component {
+  render() {
+    return (
+      <div className="newStuff">
+        <h1>New</h1>
+      </div>
+    );
+  }
+}
+
+export default New;
